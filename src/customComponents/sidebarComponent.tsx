2c7146ddf357361be83cf68cb788102ab2b28032
Yadev Jayachandran <yadev64@gmail.com>
2024-12-31 15:26:48 +0530

    Layout updates

---
+++ src/customComponents/sidebarComponent.tsx
@@ -0,0 +1,20 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar"
+ 
+export function SidebarComponent() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
--- src/layouts/mainLayout.tsx
+++ src/layouts/mainLayout.tsx
@@ -1,9 +1,29 @@
+import { SidebarComponent } from "@/customComponents/sidebarComponent";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+// import { AppSidebar } from "@/components/app-sidebar"
 import { Outlet } from "react-router-dom";
 export function MainLayout() {
   return (
     <>
-      <div>
-        <Outlet />
+      <div className="flex flex-col min-h-screen w-full">
+        <header className="border-b h-14 flex items-center px-6">
+          <h1 className="text-xl font-semibold">My App</h1>
+        </header>
+
+        <div className="flex flex-1">
+                  <SidebarProvider>
+                      
+                      <SidebarComponent />
+                      <SidebarTrigger />
+            <main className="flex-1 p-6">
+                          <Outlet />
+                          
+          </main>
+              
+            
+          </SidebarProvider>
+          
+        </div>
       </div>
     </>
   );
